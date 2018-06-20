ce0952c1acc4fd6804c51b17f2c1a56ae0402c4c
Grant Nestor <grantnestor@gmail.com>
2018-06-20 15:54:19 -0700

    Allow whitespace in mentions

--- draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
+++ draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
@@ -1,11 +1,26 @@
 /* @flow */
 
-import findWithRegex from 'find-with-regex';
 import escapeRegExp from 'lodash.escaperegexp';
 
+const findWithRegex = (regex, contentBlock, callback) => {
+  const contentBlockText = contentBlock.getText();
+  // exclude entities, when matching
+  contentBlock.findEntityRanges(
+    (character) => !character.getEntity(),
+    (nonEntityStart, nonEntityEnd) => {
+      const text = contentBlockText.slice(nonEntityStart, nonEntityEnd);
+      let matchArr;
+      let start;
+      // Go through all matches in the text and return the indices to the callback
+      while ((matchArr = regex.exec(text)) !== null) { // eslint-disable-line
+        start = nonEntityStart + matchArr.index;
+        callback(start, start + matchArr[0].length);
+      }
+    }
+  );
+};
+
 export default (trigger: string, regExp: string) => (contentBlock: Object, callback: Function) => {
-  const reg = new RegExp(String.raw({
-    raw: `(\\s|^)${escapeRegExp(trigger)}${regExp}` // eslint-disable-line no-useless-escape
-  }), 'g');
+  const reg = new RegExp(`${escapeRegExp(trigger)}[\\w\\s]{0,}`, 'g');
   findWithRegex(reg, contentBlock, callback);
 };
