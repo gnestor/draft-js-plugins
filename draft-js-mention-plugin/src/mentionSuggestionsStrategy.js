79cfb011ca54b952f92612843aba65ee6827b276
Grant Nestor <grantnestor@gmail.com>
2018-06-24 23:49:29 -0700

    Don't trigger mention plugin in code blocks

--- draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
+++ draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
@@ -3,21 +3,23 @@
 import escapeRegExp from 'lodash.escaperegexp';
 
 const findWithRegex = (regex, contentBlock, callback) => {
-  const contentBlockText = contentBlock.getText();
-  // exclude entities, when matching
-  contentBlock.findEntityRanges(
-    (character) => !character.getEntity(),
-    (nonEntityStart, nonEntityEnd) => {
-      const text = contentBlockText.slice(nonEntityStart, nonEntityEnd);
-      let matchArr;
-      let start;
-      // Go through all matches in the text and return the indices to the callback
-      while ((matchArr = regex.exec(text)) !== null) { // eslint-disable-line
-        start = nonEntityStart + matchArr.index;
-        callback(start, start + matchArr[0].length);
+  if (contentBlock.getType() !== 'code-block') {
+    const contentBlockText = contentBlock.getText();
+    // exclude entities, when matching
+    contentBlock.findEntityRanges(
+      (character) => !character.getEntity(),
+      (nonEntityStart, nonEntityEnd) => {
+        const text = contentBlockText.slice(nonEntityStart, nonEntityEnd);
+        let matchArr;
+        let start;
+        // Go through all matches in the text and return the indices to the callback
+        while ((matchArr = regex.exec(text)) !== null) { // eslint-disable-line
+          start = nonEntityStart + matchArr.index;
+          callback(start, start + matchArr[0].length);
+        }
       }
-    }
-  );
+    );
+  }
 };
 
 export default (trigger: string, regExp: string) => (contentBlock: Object, callback: Function) => {
